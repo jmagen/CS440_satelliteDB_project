d00e336eb3c6e9811f7ed67cd9a91b300f0272f8
jmagen <jmagenhej@gmail.com>
2019-03-08 22:21:42 -0800

    Update coolQueries.sql
    Adding procedures

--- src/SQL/coolQueries.sql
+++ src/SQL/coolQueries.sql
@@ -98,7 +98,22 @@ WHERE year(Launches.Dates) < 2000 AND year(Launches.Dates) > 1970
 ORDER BY Launches.Dates ASC;
 --Cost:
 
---SAME QUERIES, BUT AS PROCEDURES:
+
+
+
+--PROCEDURES:
+
+--How many satellites were launched per year?
+DELIMITER $$
+CREATE DEFINER=`cs440_magenhej`@`%` PROCEDURE `SatellitesLaunchedPerYear`()
+    NO SQL
+    COMMENT 'List number of satellites launched per year'
+SELECT year(Launches.Dates) AS Year, COUNT(*) AS Total 
+FROM Satellites INNER JOIN Launches on Launches.satId = Satellites.satId 
+GROUP BY year(Launches.Dates)$$
+DELIMITER ;
+
+--Get total number of satellites of category C operated by agency X between times i and j.
 DELIMITER $$
 CREATE DEFINER=`cs440_magenhej`@`%` PROCEDURE `CountSatellitesByCategoryAgencyTime`(IN `cat` VARCHAR(71), IN `agency` VARCHAR(4), IN `t1` YEAR, IN `t2` YEAR)
     NO SQL
@@ -112,6 +127,7 @@ GROUP BY organizationName
 ORDER BY COUNT(Operates.satId) DESC$$
 DELIMITER ;
 
+--List all satellites of category C operated by agency X between times i and j.
 DELIMITER $$
 CREATE DEFINER=`cs440_magenhej`@`%` PROCEDURE `GetSatellitesByCategoryAgencyTime`(IN `cat` VARCHAR(71), IN `agency` VARCHAR(4), IN `t1` YEAR, IN `t2` YEAR)
     NO SQL
@@ -125,3 +141,106 @@ WHERE year(Launches.Dates) <= t2 AND year(Launches.Dates) >= t1
 ORDER BY Launches.Dates ASC$$
 DELIMITER ;
 
+--Which agency operates the most satellites of Category C?
+DELIMITER $$
+CREATE DEFINER=`cs440_magenhej`@`%` PROCEDURE `RankAgenciesByCategory`(IN `cat` VARCHAR(71))
+    NO SQL
+    COMMENT 'Lists agencies by their number of satellites in the category'
+SELECT organizationName, C.category, COUNT(Operates.satId) AS Total
+FROM agencies 
+INNER JOIN Operates on agencies.orgCode = Operates.orgCode 
+INNER JOIN Launches on Operates.satId = Launches.satId
+INNER JOIN (SELECT * FROM `Contains` WHERE `Contains`.`category` = cat) C on C.satId = Launches.satId
+GROUP BY organizationName
+ORDER BY COUNT(Operates.satId) DESC$$
+DELIMITER ;
+
+--What is the average period of satellites in category C?
+DELIMITER $$
+CREATE DEFINER=`cs440_magenhej`@`%` PROCEDURE `CategoryAveragePeriod`(IN `cat` VARCHAR(71))
+    NO SQL
+SELECT C.category, AVG(Satellites.period) AS Average_Period 
+FROM (SELECT * FROM `Contains` WHERE `Contains`.`category` = cat) C
+INNER JOIN Satellites on C.satId = Satellites.satId
+GROUP BY C.category
+ORDER BY AVG(Satellites.period) DESC$$
+DELIMITER ;
+
+--How many satellites were launched by agency X between time i and j?
+DELIMITER $$
+CREATE DEFINER=`cs440_magenhej`@`%` PROCEDURE `SatellitesLaunchedByAgencyBetweenTimes`(IN `agency` VARCHAR(4), IN `t1` YEAR, IN `t2` YEAR)
+    NO SQL
+    COMMENT 'List satellites launched by agency between times t1 and t2'
+SELECT A.organizationName, Satellites.satname, Launches.Dates
+FROM (SELECT * FROM agencies WHERE agencies.orgCode = agency) A 
+INNER JOIN Operates on A.orgCode = Operates.orgCode 
+INNER JOIN Launches on Operates.satId = Launches.satId
+INNER JOIN Satellites ON Launches.satId = Satellites.satId
+WHERE year(Launches.Dates) <= t2 AND year(Launches.Dates) >= t1
+ORDER BY Launches.Dates ASC$$
+DELIMITER ;
+
+--How many satellites of category C were launched between time i and j?
+DELIMITER $$
+CREATE DEFINER=`cs440_magenhej`@`%` PROCEDURE `CountSatellitesOfCategoryLaunchedBetweenTimes`(IN `cat` VARCHAR(71), IN `t1` YEAR, IN `t2` YEAR)
+    NO SQL
+    COMMENT 'Get count of satellites of category cat launched between t1 & t2'
+SELECT C.category, COUNT(Launches.satId) AS Total
+FROM Satellites
+INNER JOIN (SELECT * FROM `Contains` WHERE `Contains`.category = cat) C on C.satId = Satellites.satId
+INNER JOIN Launches on Satellites.satId = Launches.satId
+WHERE year(Launches.Dates) <= t2 AND year(Launches.Dates) >= t1
+GROUP BY C.category
+ORDER BY COUNT(Launches.satId) ASC$$
+DELIMITER ;
+
+--Get all satellites launched between times i and j
+DELIMITER $$
+CREATE DEFINER=`cs440_magenhej`@`%` PROCEDURE `AllSatellitesLaunchedBetweenTimes`(IN `t1` YEAR, IN `t2` YEAR)
+    NO SQL
+SELECT Satellites.satname, Launches.Dates
+FROM Satellites
+INNER JOIN Launches on Satellites.satId = Launches.satId
+WHERE year(Launches.Dates) <= t2 AND year(Launches.Dates) >= t1
+ORDER BY Launches.Dates ASC$$
+DELIMITER ;
+
+--How many satellites were launched between time i and j?
+DELIMITER $$
+CREATE DEFINER=`cs440_magenhej`@`%` PROCEDURE `CountAllSatellitesLaunchedBetweenTimes`(IN `t1` YEAR, IN `t2` YEAR)
+    NO SQL
+    COMMENT 'Counts the number of satellites launched between t1 and t2'
+SELECT COUNT(Launches.satId) AS Total
+FROM Satellites
+INNER JOIN Launches on Satellites.satId = Launches.satId
+WHERE year(Launches.Dates) <= t2 AND year(Launches.Dates) >= t1
+ORDER BY Launches.Dates ASC$$
+DELIMITER ;
+
+
+--Which satellites of category C were launched between time i and j?
+DELIMITER $$
+CREATE DEFINER=`cs440_magenhej`@`%` PROCEDURE `SatellitesOfCategoryLaunchedBetweenTimes`(IN `cat` VARCHAR(71), IN `t1` YEAR, IN `t2` YEAR)
+    NO SQL
+    COMMENT 'List satellites of category cat launched between t1 and t2'
+SELECT C.category, Satellites.satname, Launches.Dates
+FROM Satellites
+INNER JOIN (SELECT * FROM `Contains` WHERE `Contains`.category = cat) C on C.satId = Satellites.satId
+INNER JOIN Launches on Satellites.satId = Launches.satId
+WHERE year(Launches.Dates) <= t2 AND year(Launches.Dates) >= t1
+ORDER BY Launches.Dates ASC$$
+DELIMITER ;
+
+--Which categories of satellites are operated by agency X?
+DELIMITER $$
+CREATE DEFINER=`cs440_magenhej`@`%` PROCEDURE `CategoriesOperatedByAgency`(IN `org` VARCHAR(4))
+    NO SQL
+    COMMENT 'List categories of satellites owned by agency'
+SELECT DISTINCT `Contains`.category, COUNT(Operates.satId) AS Total
+FROM (SELECT * FROM agencies WHERE agencies.orgCode = org) A 
+INNER JOIN Operates on A.orgCode = Operates.orgCode 
+INNER JOIN Launches on Operates.satId = Launches.satId
+INNER JOIN `Contains` on `Contains`.satId = Launches.satId
+GROUP BY `Contains`.`category`
+ORDER BY COUNT(Operates.satId) DESC$$
+DELIMITER ;

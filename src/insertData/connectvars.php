b4957f65439bc2f7fd1cb2200a0114c8bcdd0700
hutchico <hutchico@oregonstate.edu>
2019-03-14 21:37:21 -0700

    removed insert page

--- src/insertData/connectvars.php
+++
@@ -1,9 +0,0 @@
-<?php
-  // Define database connection constants
-  define('DB_HOST', 'classmysql.engr.oregonstate.edu');
-  define('DB_USER', 'cs440_hutchico');
-  define('DB_PASSWORD', '4827');
-  define('DB_NAME', 'cs440_hutchico');
-  define('CON_STRING', 'mysql:host=classmysql.engr.oregonstate.edu;dname=cs440_hutchico');
-?>
-
